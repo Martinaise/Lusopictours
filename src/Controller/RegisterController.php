fc0c26080be08ad6dead3ddb5e308007dba892d2
Martinaise <Martinaisenga@gmail.com>
2022-08-20 14:31:05 +0200

    create registersystem

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    // pour communiquer avec la bdd on declarre une variable
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager=$entityManager;
+        
+    }
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request,UserPasswordEncoderInterface $encoder): Response
+    {
+        $user=new User();
+        $form = $this->createform(RegisterType::class,$user);
+        // soummison du formulaire
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user=$form->getData();
+            // encoder le mot de passe (=crypter)
+            $password = $encoder->encodePassword($user,$user->getPassword());
+            // mettre le nouveau mot de passe crypter
+             $user->setPassword($password);
+            //  envoyer le nouveau utilisateur dans la bdd
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        return $this->render('register/index.html.twig',[
+            'formulaire'=> $form->createView()
+        ] );
+    }
+}
