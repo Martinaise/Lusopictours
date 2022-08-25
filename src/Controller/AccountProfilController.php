73490ac9299e722f3a0008f1079b48699712cc5a
Martinaise <Martinaisenga@gmail.com>
2022-08-25 23:41:08 +0200

    database connection

--- src/Controller/AccountProfilController.php
+++ src/Controller/AccountProfilController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Form\ChangeProfilType;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
